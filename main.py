0f8f49e7067b7b5fd381181ee769756487fa4d6e
TusharSharma55 <tushar.sharma@fiftyfivetech.io>
2023-12-12 17:08:19 +0530

    Added Cors

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import uvicorn
 from fastapi import FastAPI, Body
 from models import InputModel
+from fastapi.middleware.cors import CORSMiddleware
+
 from final_custom_chatbot import Assistant
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/user-input")
@@ -19,3 +29,4 @@ async def receive_user_input(question: InputModel):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=50388)
+    # uvicorn.run(app, host="127.0.0.1", port=50388)
