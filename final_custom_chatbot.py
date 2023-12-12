16fd16eea8ccf7aafcce1eafac3fc57885acfab2
TusharSharma55 <tushar.sharma@fiftyfivetech.io>
2023-12-12 17:42:49 +0530

    Added Openai API Key

--- final_custom_chatbot.py
+++ final_custom_chatbot.py
@@ -15,7 +15,7 @@ from openai import OpenAI
 class Assistant:
 
     def __init__(self):
-        self.OPENAI_API_KEY = "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"
+        self.OPENAI_API_KEY = "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"
         self.client = OpenAI(api_key=self.OPENAI_API_KEY)
 
     def run_parallel_exec(self, exec_func: Callable, iterable: Iterable, *func_args, **kwargs):
